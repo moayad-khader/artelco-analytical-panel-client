67ad65aad0a149915db1fe75edf1ae9ec1500940
moayad_khader <moayadkhader99@gmail.com>
2023-07-23 22:04:15 +0300

    upd

--- src/components/sidebars/composer.sidebar.tsx
+++ src/components/sidebars/composer.sidebar.tsx
@@ -1,16 +1,12 @@
 import { useMemo } from "react";
 
-import FolderTree, { testData } from 'react-folder-tree';
 
 const SideBar = ({ }) => {
     const onTreeStateChange = (state, event) => console.log(state, event);
 
   return (
     <div className="flex flex-col justify-between items-center w-full h-full max-h-full  text-gray-400 border-r-2 border-gray-800 shadow-xl  bg-black shadow-xl ">
-     <FolderTree
-      data={ testData }
-      onChange={ onTreeStateChange }
-    />
+ 
     </div>
   );
 };
